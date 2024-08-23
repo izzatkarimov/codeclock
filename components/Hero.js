a02a538f74f248edf1536161cfdac7908ceec497
izzat-karimov <izzatcodes@gmail.com>
2024-08-23 17:31:37 +0200

    changes

--- components/Hero.js
+++ components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
   return (
     <div className='py-4 sm:py-12 flex flex-col gap-8 sm:gap-10 '>
       <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + orbitron.className}><span className="textGradient">CodeClock</span> is your ultimate tool to track your daily <span className="textGradient">Coding</span> habits!</h1>
-      <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px] '>Create your coding tracker and see your <span className="font-semibold">progress</span></p>
+      <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[800px] '>Track your daily coding progress with <span className="font-semibold"> ease</span> and <span className="font-semibold"> simplicity</span></p>
       <CallToAction />
       <Calendar demo/>
     </div>
